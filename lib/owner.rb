9e67f4c11a90bc3ec1f7b3bae7b2ed9a62f5b093
Karla R <karla.y.reyes12@gmail.com>
2018-07-19 14:50:09 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,58 @@
 class Owner
-  # code goes here
+  
+  attr_accessor :pets, :name
+  attr_reader :species
+  @@all = []
+  @@count = 0
+  
+  
+  def initialize(name)
+    @pets = {fishes: [], cats: [], dogs: []}
+    @@all << self
+    @@count += 1
+    @species = "human"
+  end
+  
+  def say_species
+    "I am a human."
+  end
+
+    
+  
+  def buy_cat(name)
+    name = Cat.new(name)
+    @pets[:cats] << name
+  end
+
+  def buy_dog(name)
+    name = Dog.new(name)
+    @pets[:dogs] << name
+end
+  
+  def buy_fish(name)
+    name = Fish.new(name)
+    @pets[:fishes] << name
+  end
+  
+  def walk_dogs
+    @pets[:dogs].map {|dog| dog.mood=("happy")}
+end
+
+  def play_cats
+    
+
+
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.reset_all
+    @@count = 0
+  end
+  
+  
 end
